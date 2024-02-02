50d02db82685210e5eac35f6aecb3a8ba579c9bc
michele <mbarrant@iu.edu>
2024-02-01 19:17:47 -0500

    pushin

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = 3000; 
+
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Arithmetic service - Hello World!');
+});
+
+app.get('/add/:n/:m', (req, res) => {
+    res.json(Number(req.params.n) + Number(req.params.m))
+})
+
+
+app.listen(port); 
