31b102ea8e13b25d3f1ba43a3476ef863635ee77
michele <mbarrant@iu.edu>
2024-02-15 19:27:03 -0500

    minor changes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express();
 const cors = require('cors');
 const port = 3000; 
+const {add} = require("./arithmetic")
 
 app.use(cors());
 
@@ -13,8 +14,11 @@ app.get('/', (req, res) => {
 });
 
 app.get('/add/:n/:m', (req, res) => {
-    res.json(Number(req.params.n) + Number(req.params.m))
-})
+    let n = Number(req.params.n);
+    let m = Number(req,params.m);
+    let sum = add(n,m);
+        res.json(sum);
+});
 
 
 app.listen(port); 
---
+++ src/test/arithmetic.test.js
@@ -0,0 +1,9 @@
+const {add} = require("../arithmetic")
+
+test('2 + 3 is equal to 5', () => {
+    expect(add(2,3).toBe(5)
+    )});
+
+    test('65 + 4 is equal to 69', () => {
+        expect(add(65, 4)).toBe(69)
+    })
